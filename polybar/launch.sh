110be117998b2755d826e771394050836c0fb382
Abdisalan Mohamud <abdi.mohamud94@gmail.com>
2021-07-30 00:26:15 -0600

    replaced convoluted bar with simple example

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,12 +1,13 @@
-#!/usr/bin/env sh
-
-## Add this to your wm startup file.
+#!/usr/bin/zsh
 
 # Terminate already running bar instances
 killall -q polybar
-
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 
 # Launch bar1 and bar2
-polybar -c ~/.config/polybar/config.ini main &
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
+# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
+
+echo "Bars launched..."
